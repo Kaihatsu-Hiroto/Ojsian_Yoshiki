1944ea61ee8fc74738e7daab915fbee657cb19f8
DA\nwuser <noppo.kappa@gmail.com>
2017-06-02 11:54:52 +0900

    ＜ワニのスクリプト＞※まだ未記入

---
+++ Assets/Script/Ccrocodile/Crocodile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crocodile : MonoBehaviour {
+
+    public Frog m_frog { get;private set;}
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+
+	
+    void UpdateByFrame()
+    {
+        Vector3 WaniPos = FrogManager.Instance.
+            m_frogController.m_frog.transform.position;
+
+        if (WaniPos.x < 0)
+            transform.position = new Vector3(WaniPos.x + 5, 0, -10);
+
+        if (WaniPos.x > 0)
+        {
+            transform.position = new Vector3(WaniPos.x + 5, 0, -10);
+        }
+    }
+}
+
